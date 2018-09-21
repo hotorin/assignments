22afd058e5d803c2c27326c558f379ae593e7805
Srettha Terananont(win) <strayedpeople@gmail.com>
2018-09-21 12:47:10 +0700

    remove unused code

--- feed/index.js
+++ feed/index.js
@@ -10,15 +10,15 @@ app.set('views', `${__dirname}/views`);
 app.set('view engine', 'pug')
 
 app.get('/', async (req, res) => {
-    const { data } = axios.get(`${BACKEND_URL}`);
+    const { data } = axios.get(`${BASE_BACKEND_URL}`);
 
     res.render('index', {
-        subreddits: subreddits
+        subreddits: data.subreddits
     });
 });
 
 app.get('/rank', async (req, res) => {
-    const { data } = axios.get(`${BACKEND_URL}`);
+    const { data } = axios.get(`${BASE_BACKEND_URL}`);
 
     res.render('rank', {
         rank: data.rank
